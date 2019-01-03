96bc4942d37a4025cdcc844819f019415b7d5d79
JTL-SW-SCH\shicaihang <shicaihang@goldmantis.com>
2019-01-03 14:57:15 +0800

    update template.ts

--- src/template.ts
+++ src/template.ts
@@ -1,22 +1,20 @@
 
 function identity<T> (arg: T): T {return arg;};
 
-function logIdentity<T> (arg: Array<T>): Array<T> {console.log(arg.length); return arg;};
+function logIdentity<T> (arg: T[]): T[] {console.log(arg.length); return arg;};
 
 let myIdentity: <T>(arg: T) => T = identity;
 
-let myIdentity2: {<T>(arg: T): T} = identity; // 调用签名对象字面量定义泛型
+type IdentityInterface<S> = <T>(arg: T) => T;
 
-interface myIdentityInterface<T> {<T>(arg: T): T};
-
-let myIdentity3: myIdentityInterface<string> = identity; // 调用泛型接口定义泛型
+let myIdentity3: IdentityInterface<string> = identity; // 调用泛型接口定义泛型
 
 myIdentity3('string');
 
 // 泛型类
 class GenericNumber<T> {
-  zeroValue: T;
-  add: (x: T, y: T) => T;
+  public zeroValue: T;
+  public add: (x: T, y: T) => T;
 }
 
 let myGenericNumber = new GenericNumber<number>();
